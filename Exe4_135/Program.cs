5cadd834d75d0cb00d77b60de2f4919c515ca1e8
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-12-07 15:35:09 +0700

    declare variables and add method Stacks

--- Exe4_135/Program.cs
+++ Exe4_135/Program.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace Exe4_135
 {
-    internal class Program
+    class Stacks
     {
+        private char[] dimas;
+        private int top;
+        private int max;
+        private int n;
+        public Stacks()
+        {
+            dimas = new char[26];
+            top = -1;
+            max = 26;
+        }
+
         static void Main(string[] args)
         {
         }
